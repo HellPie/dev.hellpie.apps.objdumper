b5adbff15134d76e495f1bf16693d1fa02ab8029
HellPie <hellpiedev+github@gmail.com>
2015-11-20 21:51:53 +0100

    Added Floating Action Button and fixed UI Bugs
    Added:
    - Floating Action Button! (On Header/CollapsingToolbarLayout)
    - Backround color to Theme (see styles.xml and colors.xml)
    
    Fixed:
    - CollapsingToolbarLayout title is no longer primary text color (see styles.xml and activity_main.xml)
    - Removed some colors that were not being used (see colors.xml)
    
    Still to fix:
    - Google sucks at making decent docs for their support libraries

--- app/src/main/java/dev/hellpie/apps/objdumper/MainActivity.java
+++ app/src/main/java/dev/hellpie/apps/objdumper/MainActivity.java
@@ -18,6 +18,7 @@ package dev.hellpie.apps.objdumper;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +26,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
     }
 }
